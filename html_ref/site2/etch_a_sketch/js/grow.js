4c8954ec9f2220472a07397a64bdf487f9250e75
luke <LukeRocheDev@gmail.com>
2023-02-10 09:21:23 -0500

    sketchbook updates

--- html_ref/site2/etch_a_sketch/js/grow.js
+++ html_ref/site2/etch_a_sketch/js/grow.js
@@ -1,42 +1,53 @@
 
 
-grid_size_r = parseFloat(document.querySelector("#grid_size").value)
-grid_size_c = parseFloat(document.querySelector("#grid_size").value)
+function generate_grid(){
+    grid_size_r = parseFloat(document.querySelector("#grid_size").value)
+    grid_size_c = parseFloat(document.querySelector("#grid_size").value)
 
-grid = document.querySelector("#sketch")
-var style = document.defaultView.getComputedStyle(grid, null);
-sketch_width = parseFloat(style.width.slice(0, -2))
-sketch_height = parseFloat(style.height.slice(0, -2))
-console.log(style.width.slice(0, -2))
-cell_width = Math.ceil(sketch_width / grid_size_c);
-cell_height = Math.ceil(sketch_height / grid_size_r);
-console.log(sketch_width)
-console.log(sketch_height)
-console.log(grid_size_c)
-console.log(cell_width)
-console.log(cell_height)
-for(let i = cell_height; i <= sketch_height; i+=cell_height){
-    new_row = document.createElement('div');
-    new_row.classList.add('row');
-    grid.appendChild(new_row)
-    for(let j = cell_width; j <= sketch_width; j+=cell_width){
-        new_cell = document.createElement('div');
-        new_cell.classList.add('item');
-        new_cell.style.width = cell_width;
-        new_cell.style.height = cell_height;
-        new_cell.style.cssText = `width: ${cell_width}px; height: ${cell_height}px;2px solid black`;
-        new_row.appendChild(new_cell);   
+    grid = document.querySelector("#sketch")
+    var style = document.defaultView.getComputedStyle(grid, null);
+    sketch_width = parseFloat(style.width.slice(0, -2))
+    sketch_height = parseFloat(style.height.slice(0, -2))
+    console.log(style.width.slice(0, -2))
+    cell_width = (sketch_width / grid_size_c);
+    cell_height =(sketch_height / grid_size_r);
+    console.log(sketch_width)
+    console.log(sketch_height)
+    console.log(grid_size_c)
+    console.log(cell_width)
+    console.log(cell_height)
+
+    aspect_ratio = sketch_height / sketch_width
+    cell_width *= aspect_ratio
+    cell_height *= aspect_ratio
+    for(let i = cell_height; i <= sketch_height; i+=cell_height){
+        new_row = document.createElement('div');
+        new_row.classList.add('row');
+        grid.appendChild(new_row)
+        for(let j = cell_width; j <= sketch_width; j+=cell_width){
+            new_cell = document.createElement('div');
+            new_cell.classList.add('item');
+            new_cell.style.width = cell_width;
+            new_cell.style.height = cell_height;
+            new_cell.style.cssText = `width: ${cell_width}px; height: ${cell_height}px;2px solid black`;
+            new_row.appendChild(new_cell);   
+        }
     }
 }
 
+function delete_grid(grid){
+    for(const item of grid)
+        item.getParentNode().removeChild(item)
+}
 
-
+generate_grid()
 cells = document.querySelectorAll("#sketch .item")
 cells.forEach((cell) => {
 
      // and for each one we add a 'hover' listener
-     cell.addEventListener('mouseover', () => {
+     cell.addEventListener('mousemove', () => {
         cell.style.backgroundColor = "black";
+        cell.pro
     });
 
     cell.addEventListener('mouseout', () => {
@@ -50,4 +61,8 @@ rangeInput.addEventListener("change", function() {
     txt.textContent = rangeInput.value
     console.log(rangeInput.value)
     rangeInput.appendChild(txt)
+    delete_grid(cells)
+
 }, false);
+
+
