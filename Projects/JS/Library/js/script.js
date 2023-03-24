53eabcb7b0a06b2c93a4e32d6bfd11a50ef8df2d
compiler <lukerochedev@gmail.com>
2023-03-24 08:49:03 -0400

    class library

--- Projects/JS/Library/js/script.js
+++ Projects/JS/Library/js/script.js
@@ -1,80 +1,91 @@
 
-let my_library = [];
-const num_books_per_shelf = 3;
 
 
-function Book(title, author, page_count){
-    this.title = title;
-    this.author = author;
-    this.page_count = page_count;
+class Book{
+    constructor(title, author, page_count){
+        this.title = title;
+        this.author = author
+        this.page_count = page_count
+    }
+    add_to_DOM(){
+        book = document.createElement('div')
+        book.textContent = `${this.title} by ${this.author}.`
+        book.classList.add('book')
+        return book
+    }
+}
+class Library{
+    constructor(){
+        this.my_library = [];
+        this.num_books_per_shelf = 3;
+        this.bookcase = document.querySelector(".bookcase");
+
+    }
+
+    addBook(title, author, page_count){
+        this.my_library.push(new Book(title, author, page_count));
+    }
+
+    
+    populate_bookcase(){
+        let num_shelfs = this.my_library.length / this.num_books_per_shelf + this.my_library.length % this.num_books_per_shelf;
+        for(let shelf_num = 0; shelf_num < num_shelfs; shelf_num++){
+            let shelf = document.createElement('div');
+            shelf.classList.add('bookshelf')
+
+            if(shelf_num * 3 + 0 < my_library.length)
+                shelf.appendChild(my_library[shelf_num * 3 + 0].add_to_DOM());
+            if(shelf_num * 3 + 1 < my_library.length)
+                shelf.appendChild(my_library[shelf_num * 3 + 1].add_to_DOM());
+            if(shelf_num * 3 + 2 < my_library.length)
+                shelf.appendChild(my_library[shelf_num * 3 + 2].add_to_DOM());
+
+            
+            this.bookcase.appendChild(shelf)
+        }
+
+
+    }
+
+    destroy_bookcase(){
+        let parent = document.querySelector(".bookcase");
+        while (parent.firstChild) parent.removeChild(parent.firstChild);
+
+    }
+
+
 }
 
-Book.prototype.add_to_DOM = function(){
-    book = document.createElement('div')
-    book.textContent = `${this.title} by ${this.author}.`
-    book.classList.add('book')
-    return book
-}
+
+
+
+
+
+
+
+
+my_library = new Library()
+my_library.addBook("Deep Work", "Cal Newport", 200);
+my_library.addBook("12 Rules for Life", "Jordan B. Peterson", 200);
+my_library.addBook("Can't Hurt Me", "David Goggins", 200);
+my_library.addBook("Never Finished", "David Goggins", 200);
+my_library.addBook("The Talent Code", "Daniel Coyle", 200);
+my_library.addBook("Deep Work", "Cal Newport", 200);
+my_library.addBook("Deep Work", "Cal Newport", 200);
+console.log(my_library)
+my_library.populate_bookcase()
+
 
 function open_form(){
     document.querySelector("form").style.visibility = "visible";
 }
 
 function close_form(){
-    addBookToLibrary(document.querySelector("input#Text").value, "NA", "NA")
+    my_library.addBook(document.querySelector("input#Text").value, "NA", "NA")
     document.querySelector("input#Text").value = ""
     document.querySelector("form").style.visibility = "hidden";
-    console.log(my_library)
-    destroy_bookcase();
-    populate_bookcase();
+    my_library.destroy_bookcase();
+    my_library.populate_bookcase();
 }
 document.querySelector("button").addEventListener("click", open_form);
 document.querySelector("input#Submit").addEventListener("click", close_form);
-
-function addBookToLibrary(title, author, page_count){
-    my_library.push(new Book(title, author, page_count));
-
-}
-
-
-
-function populate_bookcase(){
-    const bookcase = document.querySelector(".bookcase");
-    let num_shelfs = my_library.length / num_books_per_shelf + my_library.length % num_books_per_shelf;
-    for(let shelf_num = 0; shelf_num < num_shelfs; shelf_num++){
-        shelf = document.createElement('div');
-        shelf.classList.add('bookshelf')
-
-        if(shelf_num * 3 + 0 < my_library.length)
-            shelf.appendChild(my_library[shelf_num * 3 + 0].add_to_DOM());
-        if(shelf_num * 3 + 1 < my_library.length)
-            shelf.appendChild(my_library[shelf_num * 3 + 1].add_to_DOM());
-        if(shelf_num * 3 + 2 < my_library.length)
-            shelf.appendChild(my_library[shelf_num * 3 + 2].add_to_DOM());
-
-        
-        bookcase.appendChild(shelf)
-    }
-
-
-}
-
-function destroy_bookcase(){
-    parent = document.querySelector(".bookcase");
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-
-}
-
-addBookToLibrary("Deep Work", "Cal Newport", 200);
-addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 200);
-addBookToLibrary("Can't Hurt Me", "David Goggins", 200);
-addBookToLibrary("Never Finished", "David Goggins", 200);
-addBookToLibrary("The Talent Code", "Daniel Coyle", 200);
-addBookToLibrary("Deep Work", "Cal Newport", 200);
-addBookToLibrary("Deep Work", "Cal Newport", 200);
-console.log(my_library)
-populate_bookcase();
-
-
